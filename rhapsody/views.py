d979d1acfa114ca529e1b595befb60d64b9cc438
fologne <gabrielfranfoloni01@gmail.com>
2026-03-26 14:24:00 -0400

    Adicao de templates e /docs

--- rhapsody/views.py
+++ rhapsody/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from produto.models import Produto
 
 def visualizarHome(request):
-    return HttpResponse('<h1>Visualisando a home</h1>')
+    produtos = Produto.objects.all().filter(esta_disponivel=True)
+    context = {
+        'produtos':produtos
+    }
+    return render(request, 'index.html', context)
